a134461e3d3cbccb683b44392dfe8cce6f761639
Peichun Hua <huap@wustl.edu>
2025-08-12 17:25:38 -0500

    Configuration

--- JailGuard/main_img.py
+++ JailGuard/main_img.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import sys
 sys.path.append('./utils')
 from utils import *
--- JailGuard/utils/minigpt_utils.py
+++ JailGuard/utils/minigpt_utils.py
@@ -8,11 +8,11 @@ import torch
 import torch.backends.cudnn as cudnn
 import gradio as gr
 import sys
-sys.path.append('YOUR MINIGPT4 DIR')
+sys.path.append('../MiniGPT-4')
 from minigpt4.common.config import Config
 from minigpt4.common.dist_utils import get_rank
 from minigpt4.common.registry import registry
-from minigpt4.conversation.conversation import Chat, CONV_VISION
+from minigpt4.conversation.conversation import Chat, CONV_VISION_Vicuna0
 
 from PIL import Image
 
@@ -113,7 +113,7 @@ def initialize_model():
         "change to --cfg-options instead.",
     )
     parser.add_argument("--gpu_id", type=str, default='0', help="specify the gpu to load the model.")
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
     cfg = Config(args)
     os.environ["CUDA_VISIBLE_DEVICES"]=f'{args.gpu_id}'
     model_config = cfg.model_cfg
@@ -134,7 +134,7 @@ def initialize_model():
 def model_inference(vis_processor,chat,model,prompts_eval):
     # init chat state
     def upload_img(img):
-        chat_state = CONV_VISION.copy()
+        chat_state = CONV_VISION_Vicuna0.copy()
         img_list = []
         chat.upload_img(img, chat_state, img_list)
         return chat_state, img_list
